37feda22ce8277bcdd304f73402550bf7caf6d0a
wu0792 <wu0792@163.com>
2018-10-09 07:41:25 +0800

    use middleware

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
 const Koa = require('koa')
 const app = new Koa()
 
-app.use(async (ctx, next) => {
-    ctx.body = 'Hi wu0792'
-})
+const mid1 = async (ctx, next) => {
+    ctx.type = 'text/html'
+    await next()
+    ctx.body += 'yes'
+}
+
+const mid2 = async (ctx, next) => {
+    ctx.body = 'hi '
+    await next()
+}
+
+const mid3 = async (ctx, next) => {
+    ctx.body += 'wu0792'
+}
+
+app.use(mid1)
+app.use(mid2)
+app.use(mid3)
 
 app.listen(2333)
